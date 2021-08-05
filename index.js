d835214eb6eeb0dd23092d9c67950f57658535f6
louishu17 <louis.hu@duke.edu>
2021-08-04 23:42:36 -0400

    uibuild

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const Note = require('./models/note');
 
 app.use(express.static('build'));
 app.use(cors());
@@ -16,39 +18,15 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger);
 
-let notes = [
-	{
-		id: 1,
-		content: 'HTML is easy',
-		date: '2020-01-10T17:30:31.098Z',
-		important: true,
-	},
-	{
-		id: 2,
-		content: 'Browser can execute only Javascript',
-		date: '2020-01-10T18:39:34.091Z',
-		important: false,
-	},
-	{
-		id: 3,
-		content: 'GET and POST are the most important methods of HTTP protocol',
-		date: '2020-01-10T19:20:14.298Z',
-		important: true,
-	},
-];
-
 app.get('/', (req, res) => {
 	res.send('<h1>Hello World!</h1>');
 });
 
-app.get('/api/notes', (req, res) => {
-	res.json(notes);
+app.get('/api/notes', (request, response) => {
+	Note.find({}).then((notes) => {
+		response.json(notes);
+	});
 });
-
-const generateId = () => {
-	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-	return maxId + 1;
-};
 
 app.post('/api/notes', (request, response) => {
 	const body = request.body;
@@ -59,27 +37,21 @@ app.post('/api/notes', (request, response) => {
 		});
 	}
 
-	const note = {
+	const note = new Note({
 		content: body.content,
 		important: body.important || false,
 		date: new Date(),
-		id: generateId(),
-	};
+	});
 
-	notes = notes.concat(note);
-
-	response.json(note);
+	note.save().then((savedNote) => {
+		response.json(savedNote);
+	});
 });
 
 app.get('/api/notes/:id', (request, response) => {
-	const id = Number(request.params.id);
-	const note = notes.find((note) => note.id === id);
-
-	if (note) {
+	Note.findById(request.params.id).then((note) => {
 		response.json(note);
-	} else {
-		response.status(404).end();
-	}
+	});
 });
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -95,7 +67,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
